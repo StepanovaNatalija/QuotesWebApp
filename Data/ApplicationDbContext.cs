aced47dd3a174b409be0fb3c458dac7bc074a619
StepanovaNatalija <stepanova@natalie@gmail.com>
2021-12-12 00:36:15 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using QuotesWebApp.Models;
+
+namespace QuotesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<QuotesWebApp.Models.Quote> Quote { get; set; }
+    }
+}
---
+++ Models/Quote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QuotesWebApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string QuoteText { get; set; }
+        public string QuoteAuthor { get; set; }
+
+        public Quote()
+        {
+
+        }
+
+
+    }
+
+
+}
